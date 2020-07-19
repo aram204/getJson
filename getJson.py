a0f18508aee7d1497970f8b2ecdd5c78e9e0d748
aram2004 <arsarg6440@gmail.com>
2020-07-19 14:35:51 +0400

    get json

---
+++ getJson.py
@@ -0,0 +1,8 @@
+from urllib.request import urlopen
+import json
+with urlopen("https://swapi.dev/api/people/1/") as f:
+    source = f.read()
+data = json.loads(source)
+html = json.dumps(data, indent = 2)
+with open("file.json","w") as f:
+    f.write(html)
